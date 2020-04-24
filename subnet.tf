36d4dd3dd7a452083b8ae8d3b6ddd74e1a69ee82
enrique <esv@localhost.localdomain>
2020-04-24 07:50:50 -0400

    added a internet gateway and first stab at looping around available zones

--- subnet.tf
+++ subnet.tf
@@ -5,14 +5,17 @@ data "aws_availability_zones" "available" {
 }
 
 
-resource "aws_subnet" "public_0" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "public" {
+
+  count = length(aws_availability_zones.available)
+
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.0.0.${count.index}/24"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_1" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.main_vpc.id
   cidr_block = "10.0.0.0/24"
   map_public_ip_on_launch = true
 }
