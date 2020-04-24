7a7c4052e482f4127529f8c1b4b112eb21229b3e
enrique <esv@localhost.localdomain>
2020-04-24 07:53:18 -0400

    fixed syntax error with vpc

--- vpc.tf
+++ vpc.tf
@@ -7,8 +7,8 @@ provider "aws" {
 
 resource "aws_vpc"  "main_vpc" {
   cidr_block  = "10.0.0.0/16"
-  enable_dns_support  = True
-  enable_dns_hostnames = True
+  enable_dns_support  = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "gw" {
