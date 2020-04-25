5bd202c9a2a6ab5a15ec403c03e387fd695f55d4
enrique <esv@localhost.localdomain>
2020-04-25 05:52:29 -0400

    fixed a problem with subnets cidr spec

--- subnet.tf
+++ subnet.tf
@@ -7,10 +7,11 @@ data "aws_availability_zones" "available" {
 
 resource "aws_subnet" "public" {
 
-  count = length(data.aws_availability_zones.available)
+  count = length(data.aws_availability_zones.available.names)
 
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id = aws_vpc.main_vpc.id
-  cidr_block = "10.0.0.${count.index}/24"
+  cidr_block = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
 }
 
